3a9c2e7d55482a591b920436d492c77c37801f9b
Fahmi Nugroho <54502651+fahmi-nugroho@users.noreply.github.com>
2022-04-14 12:43:17 +0700

    update function reshape whit standarize input

--- elm.py
+++ elm.py
@@ -1,7 +1,12 @@
 import numpy as np
+import math
 from joblib import load
 
 def reshape(pregnancies, glucose, bloodPressure, skinThickness, insulin, bmi, diabetesPedigreeFunction, age):
+    total = 0
+    penyimpangan = 0
+    new_data = []
+
     test_data = [
         pregnancies, 
         glucose, 
@@ -13,10 +18,28 @@ def reshape(pregnancies, glucose, bloodPressure, skinThickness, insulin, bmi, di
         age
     ]
 
-    test_data = np.array(test_data)
-    test_data = test_data.reshape(1, -1)
+    for data in test_data:
+        total += data
 
-    return test_data
+    rata2 = total / 8
+
+    for i in range(8):
+        penyimpangan += pow(test_data[i]-rata2, 2)
+    
+    varians = penyimpangan / 8
+    std = math.sqrt(varians)
+    
+    for new in test_data:
+        new_value = (new-rata2)/std
+        new_data.append(new_value)
+
+    print('=====')
+    print(new_data)
+
+    new_data = np.array(new_data)
+    new_data = new_data.reshape(1, -1)
+
+    return new_data
 
 def input_to_hidden(data, Win, bias):
     a = np.dot(data, Win)
